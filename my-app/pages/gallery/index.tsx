c4fa6ba8001300bae34a31b981630effde79a1f5
TLRKiliann <philogenie@protonmail.com>
2023-02-05 12:36:24 +0100

    new route created

---
+++ my-app/pages/gallery/index.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router'
+import styles from '@/styles/Gallery.module.scss'
+
+const Gallery:React.FC = () => {
+  const router = useRouter()
+
+  const handleHome = () => {
+    router.replace('/')
+  }
+  return (
+    <>
+      <div>
+        <div>
+          <button type="button" onClick={handleHome}>
+            Go back to home
+          </button>
+        </div>
+        <h2>Gallery</h2>
+      </div>
+    </>
+  )
+}
+
+export default Gallery
