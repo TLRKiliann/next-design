ae70a6740ff8b2c1f853baa68564c7d7fae6ac56
TLRKiliann <philogenie@protonmail.com>
2023-02-05 22:27:42 +0100

    myprojects url added

---
+++ my-app/data/myprojects.ts
@@ -0,0 +1,32 @@
+export const myprojects = [
+  {
+    id: 1,
+    name: "NextJS API (Restfull)",
+    url: 'https://github.com/TLRKiliann/nextjs-api'
+  },
+  {
+    id: 2,
+    name: "MERN-stack Chat application (Vite)",
+    url: 'https://github.com/TLRKiliann/mernstack-api'
+  },
+  {
+    id: 3,
+    name: "e-commerce (React + Mongoose)",
+    url: 'https://github.com/TLRKiliann/typescript-e-commerce'
+  },
+  {
+    id: 4,
+    name: "Anxious-Scale Psychiatry Evaluation (Python3 standalone en + fr)",
+    url: 'https://github.com/TLRKiliann/anxious_scale'
+  },
+  {
+    id: 5,
+    name: "TimeTrack (Web App Python3 SSH SCP MySQL)",
+    url: 'https://github.com/TLRKiliann/timetrack'
+  },
+  {
+    id: 6,
+    name: "Mon premier Serveur (PhP MySQL)",
+    url: 'https://github.com/TLRKiliann/server-PHP-MySQL'
+  }
+]
