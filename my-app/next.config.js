78c459e5a48952673b677b1094bc624bc826df13
TLRKiliann <philogenie@protonmail.com>
2023-02-06 19:29:29 +0100

    option for eslint added for building app

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -3,6 +3,9 @@ const path = require('path')
 
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
